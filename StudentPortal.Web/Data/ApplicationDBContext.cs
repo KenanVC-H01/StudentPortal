4a3b06ecde22bd0adbece07ba6def9aa7c0608a8
KenanVC-H01 <vch01kenan.kodehode@gmail.com>
2024-06-10 11:25:44 +0200

    Update

---
+++ StudentPortal.Web/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentPortal.Web.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) :base(options)
+        { 
+        
+        }
+
+    }
+}
